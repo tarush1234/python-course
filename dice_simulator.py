f2a4ae218954e26b5dfc38f887cc141eb6527a85
tarush1234 <85294591+tarush1234@users.noreply.github.com>
2021-06-03 18:53:30 +0530

    Create dice_simulator.py

---
+++ dice_simulator.py
@@ -0,0 +1,9 @@
+import random
+ran = random.randint(1,10)
+print("___________________")
+print("|                  |")
+print("|                  |")
+print("|     ",ran,"      |")
+print("|                  |")
+print("|                  |")
+print("___________________")
